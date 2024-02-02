fbac94f320bcc50ef91348a1ab5cfe47d8c61b1b
unknown <hoseinrajabi1997@gmail.com>
2024-02-02 00:37:03 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lchooh chooh choooh
         </a>
       </header>
     </div>
